8e654052625acea6796d296318e5365c7d8c929c
WuBoytH <alexwuief@gmail.com>
2022-05-16 22:47:47 -0400

    updated wall_jump_check

--- src/wua_bind.rs
+++ src/wua_bind.rs
@@ -487,11 +487,15 @@ pub mod MiscModule {
 
     /// Forces a wall jump. Will be moved to FGCModule eventually.
     pub unsafe fn wall_jump_check(fighter: &mut L2CFighterCommon) {
-        if GroundModule::is_wall_touch_line(fighter.module_accessor, *GROUND_TOUCH_FLAG_RIGHT_SIDE as u32)
-        || GroundModule::is_wall_touch_line(fighter.module_accessor, *GROUND_TOUCH_FLAG_LEFT_SIDE as u32) {
-            if (ControlModule::get_command_flag_cat(fighter.module_accessor, 0) & *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP != 0
-            && ControlModule::is_enable_flick_jump(fighter.module_accessor))
-            || ControlModule::get_command_flag_cat(fighter.module_accessor, 0) & *FIGHTER_PAD_CMD_CAT1_FLAG_JUMP_BUTTON != 0 {
+        let right_side = GroundModule::is_wall_touch_line(fighter.module_accessor, *GROUND_TOUCH_FLAG_RIGHT_SIDE as u32);
+        let left_side = GroundModule::is_wall_touch_line(fighter.module_accessor, *GROUND_TOUCH_FLAG_LEFT_SIDE as u32);
+        if right_side {
+            if fighter.global_table[CMD_CAT1] & *FIGHTER_PAD_CMD_CAT1_FLAG_WALL_JUMP_LEFT != 0 {
+                StatusModule::change_status_request_from_script(fighter.module_accessor, *FIGHTER_STATUS_KIND_WALL_JUMP, true);
+            }
+        }
+        else if left_side {
+            if fighter.global_table[CMD_CAT1] & *FIGHTER_PAD_CMD_CAT1_FLAG_WALL_JUMP_RIGHT != 0 {
                 StatusModule::change_status_request_from_script(fighter.module_accessor, *FIGHTER_STATUS_KIND_WALL_JUMP, true);
             }
         }
